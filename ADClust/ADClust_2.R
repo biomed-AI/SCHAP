fe782449d8aaf1e2415892006ecb490accbfc0ad
Haokun Zhang <110893478+AdrianKonHK@users.noreply.github.com>
2023-12-29 16:06:48 +0800

    Update ADClust_2.R

--- ADClust/ADClust_2.R
+++ ADClust/ADClust_2.R
@@ -25,17 +25,10 @@ table(gsm@meta.data$SCT_snn_res.0.8)
 table(gsm@meta.data$seurat_clusters)
 Idents(gsm) <- clusters[,1]
 levels(gsm)
-#最后对聚类的不同细胞亚群进行注释
-#前面呢是标准的聚类分群，每个细胞亚群仅仅是一个编号，实际上还需要给予它们一定的生物学意义，我们这里采用SingleR的标准代码：
-# marker gene #
-###该步骤应该作为一个表格输出，预计需要输出更多marker基因？比如20个
 gsm <- PrepSCTFindMarkers(object=gsm ,assay="SCT" ,verbose=TRUE)
 gsm.markers <- FindAllMarkers(gsm, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 top10 <- gsm.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_log2FC)
 top10
-
-#############################################
-#基因富集（GO和KEGG）
 
 library(clusterProfiler)
 options(clusterProfiler.download.method = 'curl')
@@ -61,8 +54,6 @@ head(eg)
 
 geneList <- eg$ENTREZID
 
-#查看基因在聚类里的分布
-#输入能被map的marker基因
 gene <- eg$SYMBOL
 
 go <- enrichGO(gene = geneList,
@@ -109,8 +100,6 @@ head(eg)
 
 geneList <- eg$ENTREZID
 
-#查看基因在聚类里的分布
-#输入能被map的marker基因
 gene <- eg$SYMBOL
 
 go <- enrichGO(gene = geneList,
@@ -148,13 +137,7 @@ ggsave(kg_bar,filename = "kg_bar.pdf", dpi = 300 )
 ggsave(kg_dot,filename = "kg_dot.pdf", dpi = 300 )
 }
 
-
-#############################################
-#细胞注释(人和小鼠都可以)
-#这里展示的是人的分析，小鼠对应导入的RNA数据名字是另一个，mouse***的
-
 library(SingleR)
-# library(celldex)
 
 gsm_for_SingleR <- GetAssayData(gsm,slot="data")
 
@@ -210,8 +193,6 @@ ggsave(gsm_fin_umap,filename = "outputs_umap.pdf",width = 10, height = 7, units 
 gsm_fin_tsne <- DimPlot(gsm,reduction = "tsne",label=T, group.by = 'CellType')
 ggsave(gsm_fin_tsne,filename = "outputs_tsne.pdf",width = 10, height = 7, units = 'in', dpi = 300 )
 
-######提取非线性低维聚类信息######
-
 tSNE<- as.data.frame(gsm@reductions$tsne@cell.embeddings)
 UMAP<- as.data.frame(gsm@reductions$umap@cell.embeddings)
 Seurat::Idents(gsm) <- gsm$CellType
@@ -223,7 +204,6 @@ write.csv(UMAP,file="umap.csv")
 rm(tSNE,UMAP)
 gc()
 
-#用AverageHeatmap画关于marker基因的图
 library(dplyr)
 library(scRNAtoolVis)
 library(Cairo)
@@ -237,8 +217,6 @@ AverageHeatmap(object = gsm, markerGene = head(gene,60),
                column_title = NULL,row_title=NULL,assays='SCT'
                )
 dev.off()
-
-#差异分析，提取出每个细胞的差异基因，整合后画热图#
 
 for(i in 1:length(levels(gsm))) {
 data <- FindMarkers(gsm, ident.1 = gsm@meta.data$CellType[i], ident.2 = NULL, only.pos = TRUE, 
@@ -259,18 +237,12 @@ AverageHeatmap(object = gsm, markerGene = markers,
                )
 dev.off()
 
-##############################################
-#细胞通讯(使用cellchat)
-
 library(CellChat)
 library(patchwork)
 library(Seurat)
-#library(ggplot2)
 options(stringsAsFactors = FALSE)
 
-#做没做SCT的都能用这句话，因为会匹配默认的assay
 data.input = GetAssayData(gsm,slot = "data")
-
 meta.data =  gsm@meta.data
 cellchat <- createCellChat(object = data.input, 
                            meta = meta.data, 
@@ -282,8 +254,6 @@ rm(gsm)
 gc()
 groupSize <- as.numeric(table(cellchat@idents)) # number of cells in each cell group
 groupSize
-
-#加载CellChatDB数据库
 
 if (species == "Human")
 {
@@ -301,26 +271,21 @@ cellchat@DB <- CellChatDB.use
 
 cellchat <- subsetData(cellchat)  # This step is necessary even if using the whole database
 library(future)
-options(future.globals.maxSize= 40000000000) #大于3407872000即可
+options(future.globals.maxSize= 40000000000)
 future::plan("multicore", workers = 10)
 cellchat <- identifyOverExpressedGenes(cellchat)
 cellchat <- identifyOverExpressedInteractions(cellchat)
 
-#计算通讯概率，推断细胞通讯网络
 cellchat <- computeCommunProb(cellchat,population.size = F)
 # Filter out the cell-cell communication if there are only few number of cells in certain cell groups
 cellchat <- filterCommunication(cellchat, min.cells = 10)
 
 levels(cellchat@idents)
-#在信号通路水平推断细胞通讯
 cellchat <- computeCommunProbPathway(cellchat)
 head(cellchat@net)
 head(cellchat@netP)
 
-#计算加和的cell-cell通讯网络
 cellchat <- aggregateNet(cellchat)
-
-#细胞互作数量与强度统计分析
 
 groupSize <- as.numeric(table(cellchat@idents))
 library(Cairo)
@@ -334,13 +299,11 @@ dev.off()
 # save as TTL/net_number_strength.pdf
 
 # show all the significant interactions (L-R pairs)
-#需要指定受体细胞和配体细胞
 p = netVisual_bubble(cellchat, sources.use = seq(1,length(levels(cellchat@idents)),2),
                      targets.use = seq(2,length(levels(cellchat@idents)),2),,remove.isolate = FALSE,angle.x = 45)
 ggsave("bubble.pdf",p ,width = bubble_width,height = bubble_height)
 # save as TIL/Mye Lymph bubble.pdf
 
-#计算网络中心性权重
 cellchat <- netAnalysis_computeCentrality(cellchat, slot.name="netP")
 
 ##heatmap signal##
