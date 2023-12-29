df1d7f5d77d9055e24b53b3b118cda06ea55d265
Haokun Zhang <110893478+AdrianKonHK@users.noreply.github.com>
2023-12-29 16:04:11 +0800

    Update entrypoint_ADClust_2.sh

--- ADClust/entrypoint_ADClust_2.sh
+++ ADClust/entrypoint_ADClust_2.sh
@@ -5,23 +5,16 @@ set -x # debug
 set -e # exit script with any err
 # set -u # exit script with any use of empty env
 
-# 记录环境变量
 env > env.log
 
 mkdir input
 
-# 指定samples的路径
 cp $Rdata input/
 cp $clusterpred input/
 export Rdata=$(realpath input/output.Rdata)
 export clusterpred=$(realpath input/output_pred.csv)
 
-# export submitr_path=/home/zhanghaokun/ADClust_1.R
 export submitr_path=/app/common/bio-platform/SCHAP/ADClust/ADClust_2.R
-
-
-# 设置画图参数
-
 
 export pvalueCutoff=$pvalueCutoff
 export qvalueCutoff=$qvalueCutoff
@@ -30,12 +23,9 @@ export heatmap_height=$heatmap_height
 export bubble_width=$bubble_width
 export bubble_height=$bubble_height
 
-# 设置联网proxy
 export http_proxy="http://10.20.18.21:3128" && export https_proxy="http://10.20.18.21:3128"
 
-# 链接程序到指定位置
 mkdir -p /home/zhanghaokun/anaconda3/envs/
 ln -s /app/common/bio-platform/SCHAP/singler /home/zhanghaokun/anaconda3/envs/singler
 
-# 运行
 mkdir -p result && cd result && export TZ=Asia/Shanghai && /home/zhanghaokun/anaconda3/envs/singler/bin/Rscript $submitr_path $Rdata $clusterpred $pvalueCutoff $qvalueCutoff $heatmap_width $heatmap_height $bubble_width $bubble_height 2>&1 | tee ../output.log
