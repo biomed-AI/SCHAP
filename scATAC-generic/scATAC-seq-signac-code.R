2ecd868487a472e05cd4789c11f66896cacae264
Haokun Zhang <110893478+AdrianKonHK@users.noreply.github.com>
2023-12-29 15:38:53 +0800

    Update scATAC-seq-signac-code.R

--- scATAC-generic/scATAC-seq-signac-code.R
+++ scATAC-generic/scATAC-seq-signac-code.R
@@ -1,17 +1,12 @@
-#scATAC-seq细胞类型标注
 library(Signac)
 library(Seurat)
 library(GenomeInfoDb)
-#library(EnsDb.Hsapiens.v75)
-#library(EnsDb.Hsapiens.v86)
 library(patchwork)
 library(dplyr)
 library(ChIPpeakAnno)
 library(ChIPseeker)
 library(clusterProfiler)
 library(GenomicFeatures) 
-#library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-#library(org.Hs.eg.db)
 library(ggplot2)
 library(SingleCellExperiment)
 set.seed(1234)
@@ -46,30 +41,14 @@ print(h5)
 print(csv)
 print(tsv)
 
-# file1-读取Peak/Cell矩
-#Peak/Cell matrix. 该文件类似于用于分析单细胞RNA-seq的基因表达计数矩阵。然而，矩阵的每一行不是基因，而是代表一个基因组区域（“peak region”，一个峰），
 counts <- Read10X_h5(filename = h5)
 
-# 查看peaks和细胞的数目
-#dim(counts)
-#counts[1:5,1:5]
-
-# file2-读取细胞注释信息
-# 该文件储存了所有单个细胞中唯一片段（unique fragments）的完整列表。该文件非常大，处理速度较慢，并且存储在磁盘上（而不是内存中）。
-# 但是，保留此文件的优点是它包含与每个细胞相关的所有片段，而不是仅映射到峰的片段。有关片段文件的更多信息
 metadata <- read.csv(
   file = csv,
   header = TRUE,
   row.names = 1
 )
 
-# 查看metadata信息
-#names(metadata)
-#head(metadata)[,1:5]
-
-
-# ATAC-seq数据使用自定义的assay— ChromatinAssay 进行存储。这使得一些专门的功能可以用于分析单细胞的基因组assay，
-# 创建ChromatinAssay
 chrom_assay <- CreateChromatinAssay(
   counts = counts,
   sep = c(":", "-"),
@@ -79,7 +58,6 @@ chrom_assay <- CreateChromatinAssay(
   min.features = 200
 )
 
-# 构建seurat对象
 pbmc <- CreateSeuratObject(
   counts = chrom_assay,
   assay = "peaks",
@@ -87,13 +65,7 @@ pbmc <- CreateSeuratObject(
 )
 
 pbmc
-#pbmc[['peaks']]
-#granges(pbmc)
 
-
-# 可以为pbmc对象的人类基因组信息添加基因注释。这使得下游函数直接从对象中提取基因注释信息。
-#annotations <- GetGRangesFromEnsDb(ensdb = EnsDb.Hsapiens.v86)
-#annotations <- GetGRangesFromEnsDb(ensdb = EnsDb.Hsapiens.v75)
 load(anno_data)
 annotations
 # change to UCSC style since the data was mapped to hg19
@@ -128,7 +100,6 @@ VlnPlot(
 )
 dev.off()
 
-# 根据不同QC指标进行数据过滤
 pbmc <- subset(
   x = pbmc,
   subset = peak_region_fragments > 3000 &
@@ -140,11 +111,9 @@ pbmc <- subset(
 )
 pbmc
 
-# 使用RunTFIDF函数进行数据归一化
+
 pbmc <- RunTFIDF(pbmc)
-# 使用FindTopFeatures函数选择可变的features
 pbmc <- FindTopFeatures(pbmc, min.cutoff = 'q0')
-# 使用RunSVD函数进行线性降维
 pbmc <- RunSVD(pbmc)
 
 pdf("correlation_depth_components.pdf")
@@ -152,7 +121,6 @@ DepthCor(pbmc)
 dev.off()
 
 
-#非线性降维和聚类
 pbmc <- RunUMAP(object = pbmc, reduction = 'lsi', dims = 2:30)
 pbmc <- FindNeighbors(object = pbmc, reduction = 'lsi', dims = 2:30)
 pbmc <- FindClusters(object = pbmc, verbose = FALSE, algorithm = 3)
@@ -165,28 +133,3 @@ pbmc$nucleosome_group <- ifelse(pbmc$nucleosome_signal > 4, 'NS > 4', 'NS < 4')
 pdf("Fragment_length.pdf")
 FragmentHistogram(object = pbmc, group.by = 'nucleosome_group',region = FragmentHistogram_region)
 dev.off()
-
-
-
-## 创建基因表达活性矩阵（gene activity matrix）
-#gene.activities <- GeneActivity(pbmc)
-# add the gene activity matrix to the Seurat object as a new assay and normalize it 
-#pbmc[['RNA']] <- CreateAssayObject(counts = gene.activities) 
-#pbmc <- NormalizeData( 
-#  object = pbmc, 
-#  assay = 'RNA', 
-#  normalization.method = 'LogNormalize', 
-#  scale.factor = median(pbmc$nCount_RNA) 
-#)
-
-#DefaultAssay(pbmc) <- 'RNA' 
-
-#pdf("FeaturePlot.pdf",width=12, height=6)
-#FeaturePlot( 
-#  object = pbmc, 
-#  features = c('MS4A1', 'CD3D', 'LEF1', 'NKG7', 'TREM1', 'LYZ'), 
-#  pt.size = 0.1, 
-#  max.cutoff = 'q95', 
-#  ncol = 3 
-#)
-#dev.off()
