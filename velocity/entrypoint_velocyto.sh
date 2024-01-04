21a03a2554d4349c7dc032bc748ccbdcbebe6140
Haokun Zhang <110893478+AdrianKonHK@users.noreply.github.com>
2024-01-04 14:30:28 +0800

    Update entrypoint_velocyto.sh

--- velocity/entrypoint_velocyto.sh
+++ velocity/entrypoint_velocyto.sh
@@ -5,19 +5,13 @@ set -x # debug
 set -e # exit script with any err
 # set -u # exit script with any use of empty env
 
-env > env.log
-
 mkdir input
-
 
 cp $loom input/
 
 export OMP_NUM_THREADS=1
 
-
 export submitr_path=/app/common/bio-platform/SCHAP/velocyto/velocity.R
-
-export http_proxy="http://10.20.18.21:3128" && export https_proxy="http://10.20.18.21:3128"
 
 mkdir -p /home/zhanghaokun/anaconda3/envs/mamba_install/envs/
 ln -s /app/common/bio-platform/SCHAP/R4velocyto /home/zhanghaokun/anaconda3/envs/mamba_install/envs/R4velocyto
